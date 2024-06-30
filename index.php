f739be1c2c5a3e2431140884f922778e043298ff
MaksimSazanovich <sazanovichmaksim@gmail.com>
2024-06-30 20:18:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,52 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 <?php
 
-    function incrementFile($filename): int
+function incrementFile($filename): int
 {
 
-    if (file_exists($filename)) {
-        $fp = fopen($filename, "r+") or die("Failed to open the file.");
-        flock($fp, LOCK_EX);
-        $count = fread($fp, filesize($filename)) + 1;
-        ftruncate($fp, 0);
-        fseek($fp, 0);
-        fwrite($fp, $count);
-        flock($fp, LOCK_UN);
-        fclose($fp);
-    }
-    else {
-        $count = 1;
-        file_put_contents($filename, $count);
-    }
-    return $count;
+if (file_exists($filename)) {
+    $fp = fopen($filename, "r+") or die("Failed to open the file.");
+    flock($fp, LOCK_EX);
+    $count = fread($fp, filesize($filename)) + 1;
+    ftruncate($fp, 0);
+    fseek($fp, 0);
+    fwrite($fp, $count);
+    flock($fp, LOCK_UN);
+    fclose($fp);
+}
+else {
+    $count = 1;
+    file_put_contents($filename, $count);
+}
+return $count;
 }
 
 
 function convertCount($count)  {
-  $str = (string)$count; 
-  $goal = 7 - strlen($str); 
-  
-  for ($i = 0; $i < $goal; $i++) {
-    $str = "0" . $str;
-  }
+$str = (string)$count; 
+$goal = 7 - strlen($str); 
 
-  for ($i = 0; $i < strlen($str); $i++) {
-    $files = glob("images/*.*");
-    $num = $files[$str[$i]];
-	echo '<img src="'.$num.'" alt="random image">'."&nbsp;&nbsp;";
-  }
+for ($i = 0; $i < $goal; $i++) {
+$str = "0" . $str;
+}
+
+for ($i = 0; $i < strlen($str); $i++) {
+$files = glob("images/*.*");
+$num = $files[$str[$i]];
+echo '<img src="'.$num.'" alt="random image">'."&nbsp;&nbsp;";
+}
 }
 
 echo convertCount(incrementFile("views.txt"));
 ?>
+</body>
+</html>
+
 
